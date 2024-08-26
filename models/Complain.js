ea446110ba11686bedde85c58392dd7e9346b3ce
Ehtisham <b522035@iiit-bh.ac.in>
2024-08-27 00:15:25 +0530

    complain registration

--- models/Complain.js
+++ models/Complain.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-const { boolean } = require("webidl-conversions");
 const Schema = mongoose.Schema;
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-const userSchema = new Schema({
-  userId: {
-    type: Schema.type.objectId,
-    ref: "User",
+
+const ComplaintSchema = new Schema({
+  phone: {
+    type: String,
+    required: true,
   },
   discription: {
     type: String,
@@ -20,42 +18,8 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-
-  isSolved: {
-    type: boolean,
-    enum: [true, false],
-  },
-
-  // confirmPassword: {
-  //   type: String,
-  //   required: true,
-  //   validate: {
-  //     validator: function(value) {
-  //       return value === this.password;
-  //     },
-  //     message: 'Passwords do not match.'
-  //   }
-  // },
 });
-// userSchema.pre('save', function(next) {
-//   this.confirmPassword = undefined;
-// });
-// userSchema.pre("save", async function (next) {
-//   this.confirmPassword = undefined;
-//   if (!this.isModified("password")) {
-//     next();
-//   }
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
 
-// userSchema.methods.comparePassword = async function (password) {
-//   console.log(password, this.password);
-//   return await bcrypt.compare(password, this.password);
-// };
-// userSchema.methods.getJWTToken = function () {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRE * 24 * 60 * 60 * 1000,
-//   });
-// };
-const Complain = mongoose.model("Complain", userSchema);
+// Export the model
+const Complain = mongoose.model("Complain", ComplaintSchema);
 module.exports = Complain;
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const AuthRouter = express.Router();
 const { sendOtp } = require("../controller/authController");
 const { isAuthenticated } = require("../middlewares/auth");
+
 AuthRouter.post("/send-otp", sendOtp);
 AuthRouter.post("/verify-otp", isAuthenticated, (req, res) => {
   console.log("call hua");
